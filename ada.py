1125ad8ac1c67bba2329ac291eaff4d240a95e61
JuanCruzLopez07 <juancruzl2007@gmail.com>
2025-03-31 15:23:41 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,8 +1,7 @@
-def ada():
 first_name = "AdA"
 last_name = "LoVeLAce"
 name = "AdA LoVeLAce"
-namespace = "	AdA LoVeLAce"
+namespace = "  AdA LoVeLAce"
 print(name.lower())
 print(name.title())
 print(name.upper())
