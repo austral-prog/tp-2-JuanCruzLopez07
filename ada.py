4d8f2775650eee8bc564273d557180a5aa7f12da
JuanCruzLopez07 <juancruzl2007@gmail.com>
2025-03-31 15:12:44 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,8 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    name = "AdA LoVeLAce"
+    print(name.lower())
+    print(name.title())
+    print(name.upper())
+	    print(name.lower())
