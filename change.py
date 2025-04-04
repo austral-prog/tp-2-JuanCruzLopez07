2b1ec8c55304e156e332c712c35ff388b3127024
JuanCruzLopez07 <juancruzl2007@gmail.com>
2025-04-04 09:31:43 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,10 @@
 def change():
     expense = 23.75
     money = 100
+
+    print("Change")
+
+    print("Pesos:")
+    print(int(money-expense))
+    print("Centavos:")
+    print(expense-int(expense)*100)
